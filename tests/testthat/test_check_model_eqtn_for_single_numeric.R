e7e1d88163dfc0ec79588b4a5f5990b374381ab7
Michael Dougherty <mike.p.doc.71@gmail.com>
2022-03-07 13:15:50 -0600

    created check for model equations using single numeric values

---
+++ tests/testthat/test_check_model_eqtn_for_single_numeric.R
@@ -0,0 +1,26 @@
+# Get HSI models
+hsi_metadata <- nybem::hsi_metadata
+num_of_models <- length(hsi_metadata$model)
+
+# Identify HSI variables required for evaluation
+HSI_var_names <- colnames(hsi_metadata[,9:54])
+num_HSI_vars <- length(HSI_var_names)
+
+# Create a list of single numeric variables to evaluate HSI equations against
+SIV_single_numeric <- as.list(rep_len(c(1), num_HSI_vars))
+names(SIV_single_numeric) <- HSI_var_names
+
+
+test_that("check if model Eqtn can handle single numeric", {
+  for(current_model_num in 1:num_of_models) {
+    # Subset HSImetadata for current model
+    model <- hsi_metadata[current_model_num, ]
+
+    # Calculate HSI
+    hsi_result <- eval(parse(text = paste(model$Eqtn)), SIV_single_numeric)
+
+    print(paste("Model: ",current_model_num))
+
+    expect_true(is.numeric(hsi_result))
+  }
+})
