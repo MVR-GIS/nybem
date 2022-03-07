d2885655d9cbd59f2c0c04be5a0c8f3087d7f44d
Michael Dougherty <mike.p.doc.71@gmail.com>
2022-03-07 13:10:18 -0600

    check ecorest::HSImetadata$Eqtn expressions for raster compatibility

--- tests/testthat/test_check_model_eqtn_for_raster.R
+++ tests/testthat/test_check_model_eqtn_for_raster.R
@@ -9,18 +9,36 @@ num_of_models <- length(hsi_metadata$model)
 HSI_var_names <- colnames(hsi_metadata[,9:54])
 num_HSI_vars <- length(HSI_var_names)
 
-# Create a list of HSI variables to evaluate HSI equations against
-SIV <- rep_len(c(ras), num_HSI_vars)
-names(SIV) <- HSI_var_names
+# Create a list of raster HSI variables to evaluate HSI equations against
+SIV_raster <- rep_len(c(ras), num_HSI_vars)
+names(SIV_raster) <- HSI_var_names
+
+# Create a list of single numeric variables to evaluate HSI equations against
+SIV_single_numeric <- rep_len(c(ras), num_HSI_vars)
+names(SIV_single_numeric) <- HSI_var_names
 
 
-test_that("check model equations can handle rasters", {
+test_that("check if model Eqtn can handle single numeric", {
   for(current_model_num in 1:num_of_models) {
     # Subset HSImetadata for current model
     model <- hsi_metadata[current_model_num, ]
 
     # Calculate HSI
-    hsi_result <- eval(parse(text = paste(model$Eqtn)), SIV)
+    hsi_result <- eval(parse(text = paste(model$Eqtn)), SIV_single_numeric)
+
+    print(paste("Model: ",current_model_num))
+
+    expect_true(is.numeric(hsi_result))
+  }
+})
+
+test_that("check if model Eqtn can handle rasters", {
+  for(current_model_num in 1:num_of_models) {
+    # Subset HSImetadata for current model
+    model <- hsi_metadata[current_model_num, ]
+
+    # Calculate HSI
+    hsi_result <- eval(parse(text = paste(model$Eqtn)), SIV_raster)
 
     print(paste("Model: ",current_model_num))
 
