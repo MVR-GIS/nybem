dbf86776142f899e7c1969aae28323d70be660fc
Michael Dougherty <mike.p.doc.71@gmail.com>
2022-03-07 13:06:22 -0600

    created a raster-safe ifelse function

---
+++ R/ifelse.R
@@ -0,0 +1,25 @@
+#' @title Raster Con Function
+#'
+#' @description This function is designed to behave similar to the ESRI
+#'   Spatial Analyst `Con` function. The purpose of this function is to
+#'   provide R with a raster safe `ifelse` function.
+#'
+#' @export
+#' @param condition     raster or numeric; an expression that results in a
+#'                      boolean, matching the type of the condition.
+#' @param true_value
+#' @param false_value
+#'
+#' @return the true or false value matching the data type of the condition.
+#'
+#' @details This function was proposed by
+#' [Spacedman](https://gis.stackexchange.com/users/865/spacedman) on
+#' gis.stackexchange way back in 2013,
+#' [What is the equivalent of arcpy "Con" in QGIS and/or R
+#' raster-package?](https://gis.stackexchange.com/questions/69734/)
+#'
+#' @examples
+#'
+ifelse <- function(condition, true_value, false_value) {
+  return(condition * true_value + (!condition) * false_value)
+}
---
+++ tests/testthat/test_ifelse.R
@@ -0,0 +1,42 @@
+# constant rasters
+r1 <- raster(ncol = 2, nrow = 2, vals = rep_len(1, 4))
+r2 <- raster(ncol = 2, nrow = 2, vals = rep_len(2, 4))
+
+# continuous rasters
+r1_4 <- raster(ncol = 2, nrow = 2, vals = 1:4)
+
+
+test_that("check output data type", {
+  expect_true(is_RasterLayer(con_raster(r1 <  1, 0, 3)))
+  expect_true(!is_RasterLayer(con_raster(1 <  1, 0, 3)))
+  expect_true(is.numeric(con_raster(1 <  1, 0, 3)))
+})
+
+test_that("check constant raster input", {
+  expect_equal(values(con_raster(r1 <  1, 0, 3)), c(3,3,3,3))
+  expect_equal(values(con_raster(r1 == 1, 0, 3)), c(0,0,0,0))
+  expect_equal(values(con_raster(r2 <  2, 0, 3)), c(3,3,3,3))
+  expect_equal(values(con_raster(r2 == 2, 0, 3)), c(0,0,0,0))
+})
+
+test_that("check continuous raster input", {
+  expect_equal(values(con_raster(r1_4 <  2, 0, 5)), c(0,5,5,5))
+  expect_equal(values(con_raster(r1_4 == 2, 0, 5)), c(5,0,5,5))
+})
+
+test_that("check raster math operators", {
+  expect_equal(values(con_raster(r1 <  1, r1 + 1, r1 + 2)), c(3,3,3,3))
+  expect_equal(values(con_raster(r1 == 1, r1 + 1, r1 + 2)), c(2,2,2,2))
+  expect_equal(values(con_raster(r1 <  1, r1 * 2, r1 * 3)), c(3,3,3,3))
+  expect_equal(values(con_raster(r1 == 1, r1 * 2, r1 * 3)), c(2,2,2,2))
+})
+
+test_that("check raster math functions", {
+  expect_equal(values(con_raster(r1 <  1, min(r1, r2) + 1, max(r1, r2) + 1)),
+               c(3,3,3,3))
+})
+
+test_that("check single numeric input", {
+  expect_equal(con_raster(1 <  1, 0, 3), 3)
+  expect_equal(con_raster(1 == 1, 0, 3), 0)
+})
