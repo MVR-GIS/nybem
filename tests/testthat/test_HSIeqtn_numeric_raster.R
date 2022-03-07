0a6039de1ba38b30c2855dca06b01699046d0afc
Michael Dougherty <mike.p.doc.71@gmail.com>
2022-03-07 13:12:44 -0600

    simplified test raster creation syntax

--- tests/testthat/test_HSIeqtn_numeric_raster.R
+++ tests/testthat/test_HSIeqtn_numeric_raster.R
@@ -1,27 +1,15 @@
-# Barredowl observed metric values (no components)
-obs_tree_num <- raster::raster(ncol = 2, nrow = 2)
-obs_tree_num <- raster::setValues(obs_tree_num,
-                                  rep_len(1, raster::ncell(obs_tree_num)))
-obs_avg_dbh  <- raster::raster(ncol = 2, nrow = 2)
-obs_avg_dbh  <- raster::setValues(obs_avg_dbh,
-                                  rep_len(1, raster::ncell(obs_avg_dbh)))
-obs_can_cov  <- raster::raster(ncol = 2, nrow = 2)
-obs_can_cov  <- raster::setValues(obs_can_cov,
-                                  rep_len(1, raster::ncell(obs_avg_dbh)))
+# barredowl observed metric values (no components)
+obs_tree_num <- raster::raster(ncol = 2, nrow = 2, vals = rep_len(1, 4))
+obs_avg_dbh  <- raster::raster(ncol = 2, nrow = 2, vals = rep_len(1, 4))
+obs_can_cov  <- raster::raster(ncol = 2, nrow = 2, vals = rep_len(1, 4))
 
 # Combine into parameter object
 bo_test1 <- list(obs_tree_num, obs_avg_dbh, obs_can_cov)
 
-# AlewifeJuv observed metric values (two components)
-obs_zooplankton   <- raster::raster(ncol = 2, nrow = 2)
-obs_zooplankton   <- raster::setValues(obs_zooplankton,
-                                  rep_len(1, raster::ncell(obs_zooplankton)))
-sal_spring_summer <- raster::raster(ncol = 2, nrow = 2)
-sal_spring_summer <- raster::setValues(sal_spring_summer,
-                                  rep_len(1, raster::ncell(sal_spring_summer)))
-wtr_temp_surface  <- raster::raster(ncol = 2, nrow = 2)
-wtr_temp_surface  <- raster::setValues(wtr_temp_surface,
-                                  rep_len(1, raster::ncell(wtr_temp_surface)))
+# alewifeJuv observed metric values (two components)
+obs_zooplankton   <- raster::raster(ncol = 2, nrow = 2, vals = rep_len(1, 4))
+sal_spring_summer <- raster::raster(ncol = 2, nrow = 2, vals = rep_len(1, 4))
+wtr_temp_surface  <- raster::raster(ncol = 2, nrow = 2, vals = rep_len(1, 4))
 
 # Combine into parameter object
 al_test1 <- list(obs_zooplankton, sal_spring_summer, wtr_temp_surface)
@@ -30,8 +18,8 @@ al_test1 <- list(obs_zooplankton, sal_spring_summer, wtr_temp_surface)
 hsi_metadata <- ecorest::HSImetadata
 
 # Set function parameters for testing
-HSImodelname <- "barredowl"
-SIV          <- bo_test1
+HSImodelname <- "alewifeJuv"
+SIV          <- al_test1
 HSImetadata  <- hsi_metadata
 
 # Calculate SI values
