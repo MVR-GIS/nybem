c9f9afb7b5b5478d4f14c1498a10500f723dcf66
Michael Dougherty <mike.p.doc.71@gmail.com>
2022-04-23 13:41:53 -0500

    document nybem_submodels data object

---
+++ R/nybem_submodels.R
@@ -0,0 +1,21 @@
+#' NYBEM Submodels
+#'
+#' A list of submodels used in the NYBEM feasibility study.
+#'
+#' @docType data
+#'
+#' @format A list with the following named submodels:
+#' * NYBEM.fresh.tid - Freshwater, Tidal Zone model.
+#' * NYBEM.est.int - Estuarine, Intertidal Zone model.
+#' * NYBEM.est.sub.hard - Estuarine, Subtidal Zone, Hard Bottom model.
+#' * NYBEM.est.sub.soft.sav - Estuarine, Subtidal Zone, Soft Bottom, Submersed
+#'     Aquatic Vegetation model.
+#' * NYBEM.est.sub.soft.clam - Estuarine, Subtidal Zone, Soft Bottom, Hard
+#'     Clam model.
+#' * NYBEM.mar.int - Marine, Intertidal Zone model.
+#' * NYBEM.mar.sub - Marine, Subtidal Zone model.
+#' * NYBEM.mar.deep - Marine, Deepwater Zone model.
+#'
+#' @keywords datasets
+#'
+"nybem_submodels"
