8c8d590ed7b6332aca740d24b52f48f3ba31e361
Michael Dougherty <mike.p.doc.71@gmail.com>
2022-04-23 14:31:55 -0500

    added label text to nybem_submodels

--- R/nybem_submodels.R
+++ R/nybem_submodels.R
@@ -6,15 +6,25 @@
 #'
 #' @format A list with the following named submodels:
 #' * NYBEM.fresh.tid - Freshwater, Tidal Zone model.
+#' * NYBEM.fresh.tid_labels - Freshwater, Tidal Zone labels.
 #' * NYBEM.est.int - Estuarine, Intertidal Zone model.
+#' * NYBEM.est.int_labels - Estuarine, Intertidal Zone labels.
 #' * NYBEM.est.sub.hard - Estuarine, Subtidal Zone, Hard Bottom model.
+#' * NYBEM.est.sub.hard_labels - Estuarine, Subtidal Zone, Hard Bottom labels.
 #' * NYBEM.est.sub.soft.sav - Estuarine, Subtidal Zone, Soft Bottom, Submersed
 #'     Aquatic Vegetation model.
+#' * NYBEM.est.sub.soft.sav_labels - Estuarine, Subtidal Zone, Soft Bottom,
+#'     Submersed Aquatic Vegetation labels.
 #' * NYBEM.est.sub.soft.clam - Estuarine, Subtidal Zone, Soft Bottom, Hard
 #'     Clam model.
+#' * NYBEM.est.sub.soft.clam_labels - Estuarine, Subtidal Zone, Soft Bottom,
+#'     Hard Clam labels.
 #' * NYBEM.mar.int - Marine, Intertidal Zone model.
+#' * NYBEM.mar.int_labels - Marine, Intertidal Zone labels.
 #' * NYBEM.mar.sub - Marine, Subtidal Zone model.
+#' * NYBEM.mar.sub_labels - Marine, Subtidal Zone labels.
 #' * NYBEM.mar.deep - Marine, Deepwater Zone model.
+#' * NYBEM.mar.deep_labels - Marine, Deepwater Zone labels.
 #'
 #' @keywords datasets
 #'
