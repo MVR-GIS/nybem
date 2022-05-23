cab86fb7d6ee4787aa15022bfd0a6b4a264f9eb4
Michael Dougherty <mike.p.doc.71@gmail.com>
2022-05-23 10:27:04 -0500

    updated write_cat_tiff

--- R/write_cat_tiff.R
+++ R/write_cat_tiff.R
@@ -1,29 +1,33 @@
-#' @title Write Catagorical Raster TIFF file
+#' @title Write Categorical Raster TIFF file
 #'
-#' @description Writes a catagorical raster file in .tiff format with the
-#' correct raster attribute table information to be read by ESRI ArcGIS
-#' products.
+#' @description Writes a categorical raster file in .tiff format with the
+#' correct raster attribute table information and compression to be read by
+#' ESRI ArcGIS products.
 #'
 #' @export
-#' @param cat_raster   raster::Raster*; A catagorical raster.
+#' @param cat_raster   raster::Raster*; A categorical raster.
 #' @param out_dir      character; A path to a folder.
 #' @param out_name     character; A file name.
 #'
-#' @return This function is used for the side-effect of writing a catagorical
+#' @return This function is used for the side-effect of writing a categorical
 #' raster to a file.
 #'
 #' @importFrom raster writeRaster
+#' @importFrom tools file_path_sans_ext
 #' @importFrom foreign write.dbf
 #'
 write_cat_tif <- function(cat_raster, out_dir, out_name) {
   # Check inputs
-  if(!is_RasterLayer(cat_raster)) {stop("`cat_raster` must be a raster")}
+  if(!is_RasterLayer(cat_raster)) {stop("cat_raster must be a raster")}
+  if(!file.exists(out_dir)) {stop("out_dir does not exist")}
 
   # Set output names
-  out_tif <- file.path(out_dir, out_name)
-  out_dbf <- file.path(out_dir, paste0(out_name, ".tif.vat.dbf"))
-  out_cpg <- file.path(out_dir, paste0(out_name, ".tif.vat.cpg"))
-  out_clr <- file.path(out_dir, paste0(out_name, ".tif.vat.cpg"))
+  out_name_no_ext <- tools::file_path_sans_ext(out_name)
+  out_name_tif    <- paste0(out_name_no_ext, ".tif")
+  out_tif <- file.path(out_dir, out_name_tif)
+  out_dbf <- file.path(out_dir, paste0(out_name_tif, ".vat.dbf"))
+  out_cpg <- file.path(out_dir, paste0(out_name_tif, ".vat.cpg"))
+  out_clr <- file.path(out_dir, paste0(out_name_tif, ".vat.cpg"))
 
   # Save the raster
   # https://gdal.org/drivers/raster/gtiff.html
--- tests/testthat/test_write_cat_tif.R
+++ tests/testthat/test_write_cat_tif.R
@@ -2,11 +2,24 @@
 cat_ras <- raster::raster(ncol = 2, nrow = 2, vals = rep_len(1, 4))
 cat_ras <- raster::ratify(cat_ras)
 
-write_cat_tif(cat_ras, tempdir(), "cat_ras")
+write_cat_tif(cat_ras, tempdir(), "cat_ras1")
+write_cat_tif(cat_ras, tempdir(), "cat_ras2.tif")
 
 test_that("check files written", {
-  expect_true(file.exists(file.path(tempdir(), "cat_ras.tif")))
-  expect_true(file.exists(file.path(tempdir(), "cat_ras.tfw")))
-  expect_true(file.exists(file.path(tempdir(), "cat_ras.tif.vat.dbf")))
-  expect_true(file.exists(file.path(tempdir(), "cat_ras.tif.vat.cpg")))
+  expect_true(file.exists(file.path(tempdir(), "cat_ras1.tif")))
+  expect_true(file.exists(file.path(tempdir(), "cat_ras1.tfw")))
+  expect_true(file.exists(file.path(tempdir(), "cat_ras1.tif.vat.dbf")))
+  expect_true(file.exists(file.path(tempdir(), "cat_ras1.tif.vat.cpg")))
+  expect_true(file.exists(file.path(tempdir(), "cat_ras2.tif")))
+  expect_true(file.exists(file.path(tempdir(), "cat_ras2.tfw")))
+  expect_true(file.exists(file.path(tempdir(), "cat_ras2.tif.vat.dbf")))
+  expect_true(file.exists(file.path(tempdir(), "cat_ras2.tif.vat.cpg")))
 })
+
+test_that("check errors", {
+  expect_error(write_cat_tiff(7, tempdir(), "numeric"))
+  expect_error(write_cat_tiff("a", tempdir(), "character"))
+  expect_error(write_cat_tiff(cat_ras, paste0(tempdir(), "1234"),
+                              "cat_ras1"))
+})
+
