5e90f59d50f8f7390277f94962a75597ded12917
Michael Dougherty <mike.p.doc.71@gmail.com>
2022-06-06 10:20:53 -0500

    updated summarize_by_poly to handle na

--- R/summarize_by_poly.R
+++ R/summarize_by_poly.R
@@ -9,11 +9,14 @@
 #'
 #' @return A data frame containing polygons summarized by the following fields:
 #'   * hu_<model_name> - HSI score mean for the polygon.
+#'   * count_<model_name> - Count of cells for the polygon.
 #'   * acres_<model_name> - Area in acres for the polygon.
 #'
-#' @importFrom rlang enquo !! :=
+#' @importFrom rlang enquo !! := sym
 #' @importFrom raster extract area
-#' @importFrom dplyr rename mutate
+#' @importFrom terra rast linearUnits
+#' @importFrom dplyr rename mutate inner_join
+#' @importFrom magrittr %>%
 #'
 summarize_by_poly <- function(hsi_model, polys) {
   # Check inputs
@@ -35,19 +38,31 @@ summarize_by_poly <- function(hsi_model, polys) {
                            fun = mean,
                            na.rm = TRUE,
                            df = TRUE)
-  # Summarize modeled area (cell count) for each poly
-  num_cells <- lengths(raster::extract(hsi_model, polys))
+
+  # Reclassify HSI model values to 1 for counting
+  hsi_model_1 <- raster::reclassify(hsi_model,
+                                    cbind(-Inf, Inf, 1))
 
   # Calculate cell area in sq m
   cell_size_m <- terra::linearUnits(terra::rast(hsi_model))
   cell_area <- cell_size_m^2
 
-  # Join HSI and (calculate) Area
+  # Summarize modeled area for each poly
+  num_cells_df <- raster::extract(hsi_model_1, polys,
+                                  fun = sum,
+                                  na.rm = TRUE,
+                                  df = TRUE)
+
+  # Calculate modeled area
+  num_cells_df <- num_cells_df %>%
+    rename(!!cnt_field := !!model_name) %>%
+                                               # 1 sq m = 0.000247105 acres
+    mutate(!!area_field := (!!sym(cnt) * cell_area) * 0.000247105)
+
+  # Join HSI and Area dfs
   hu_df <- hu_df %>%
     rename(!!hu_field := !!model_name) %>%
-    mutate(!!cnt_field := num_cells) %>%
-                            # 1 sq m = 0.000247105 acres
-    mutate(!!area_field := (num_cells * cell_area) * 0.000247105)
+    inner_join(num_cells_df, by = "ID")
 
   return(hu_df)
 }
--- tests/testthat/test_summarize_by_poly.R
+++ tests/testthat/test_summarize_by_poly.R
@@ -9,22 +9,41 @@ model_1   <- raster::raster(ncol = 2, nrow = 2, vals = rep_len(1, 4),
 model_4   <- raster::raster(ncol = 2, nrow = 2, vals = 1:4,
                             xmn = 0, xmx = 2, ymn = 0, ymx = 2,
                             crs = "EPSG:26918")
+model_na   <- raster::raster(ncol = 2, nrow = 2, vals = c(2, 2, NA, NA),
+                            xmn = 0, xmx = 2, ymn = 0, ymx = 2,
+                            crs = "EPSG:26918")
 
 # Create test polygons
 poly_1 <- raster::rasterToPolygons(model_0, dissolve = TRUE)
 poly_4 <- raster::rasterToPolygons(model_0)
 
 # Visually verify raster and polygon dimensions
-# plot(model_4)
+# plot(model_na)
 # plot(sf::st_as_sf(poly_1), add = TRUE, col = NA, border = "red")
 
 # Calculate polygon summaries
 sum_0 <- summarize_by_poly(model_0, sf::st_as_sf(poly_1))
+sum_1 <- summarize_by_poly(model_1, sf::st_as_sf(poly_1))
+sum_2 <- summarize_by_poly(model_na, sf::st_as_sf(poly_1))
+
+# Variable for testing
+hsi_model <- model_0
+polys <- sf::st_as_sf(poly_1)
 
 # all rasters are 4 sq meters = 0.000988422 acres
 
+
 test_that("check polygon summary", {
   expect_equal(sum_0$hu_layer, 0)
+  expect_equal(sum_0$count_layer, 4)
   expect_equal(sum_0$acres_layer, 0.000988422, tolerance = 0.00001)
+  expect_equal(sum_1$hu_layer, 1)
+  expect_equal(sum_1$count_layer, 4)
+  expect_equal(sum_1$acres_layer, 0.000988422, tolerance = 0.00001)
 })
 
+test_that("check na polygon summary", {
+  expect_equal(sum_2$hu_layer, 2)
+  expect_equal(sum_2$count_layer, 2)
+  expect_equal(sum_2$acres_layer, 0.000494211, tolerance = 0.00001)
+})
