cd86b38c52083e1fe8fc695c25c5d7a25464c09b
Michael Dougherty <mike.p.doc.71@gmail.com>
2022-07-19 12:15:04 -0500

    added ability to toggle off progress bar

--- R/summarize_by_poly.R
+++ R/summarize_by_poly.R
@@ -6,6 +6,7 @@
 #' @param hsi_model   raster; A raster HSI model.
 #' @param polys       sf; A polygon used to derive be summaries from the
 #'                    raster.
+#' @param progress    logical; TRUE displays a progress bar during processing
 #'
 #' @return A data frame containing polygons summarized by the following fields:
 #'   * ID - Polygon unique identifier.
@@ -14,15 +15,16 @@
 #'   * acres_<model_name> - Area in acres for the polygon.
 #'
 #' @importFrom rlang enquo !! := sym
-#' @importFrom exactextractr  exact_extract
+#' @importFrom exactextractr exact_extract
 #' @importFrom terra rast linearUnits
 #' @importFrom dplyr rename mutate relocate
 #' @importFrom magrittr %>%
 #'
-summarize_by_poly <- function(hsi_model, polys) {
+summarize_by_poly <- function(hsi_model, polys, progress = TRUE) {
   # Check inputs
   if(!is_RasterLayer(hsi_model)) {stop("hsi_model must be a raster")}
   if(!class(polys)[1] == "sf") {stop("polys must be an sf object")}
+  if(!is.logical(progress) == TRUE) {stop("progress must be logical")}
 
   # Construct and enquote field names
   model <- names(hsi_model)
@@ -36,10 +38,12 @@ summarize_by_poly <- function(hsi_model, polys) {
 
   # Summarize model for each poly
   sum_df <- exactextractr::exact_extract(hsi_model, polys,
-                                         fun = c("mean", "count"))
+                                         fun = c("mean", "count"),
+                                         progress = progress)
 
   # Calculate cell area in m sq
-  cell_size_m  <- terra::linearUnits(terra::rast(hsi_model))
+  terra_rast <- terra::rast(hsi_model)
+  cell_size_m  <- terra::linearUnits(terra_rast)
   cell_area_m2 <- cell_size_m^2
 
   # Rename fields and calculate area in acres
